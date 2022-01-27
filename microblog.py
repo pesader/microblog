694bdb1c00432c19d014730efbdf6d6f060b73af
Pedro Sader Azevedo <pedro.saderazevedo@gmail.com>
2022-01-27 17:35:19 -0300

    implement posts

--- microblog.py
+++ microblog.py
@@ -1,2 +1,10 @@
-from app import app
-# from package named "app" import the Flask class instance variable (also) named "app"
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    # run "flask shell" in a terminal to get a python interpreter pre loaded
+    # with definitions related to the context of your Flask project (the ones
+    # defined below
+    return {'db': db, 'User': User, 'Post': Post}
+
